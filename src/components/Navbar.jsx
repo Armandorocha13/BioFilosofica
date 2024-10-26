71a5b7ff24ee4a92104bf5a41f7d7109070a45d1
Dev Armando Rocha <mandoqxo@gmail.com>
2024-10-26 00:44:05 -0300

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+// src/components/Navbar.js
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <h1>Bio Filosófica</h1>
+    </div>
+    
+  );
+}
+
+export default Navbar;
